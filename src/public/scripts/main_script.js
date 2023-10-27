7358a56244443f0319cfb1f6a17bd8173904d1e9
Nazareno Tognoli <nazarenotognoli@gmail.com>
2023-10-26 21:04:15 -0300

    Se cambio la lógica de como se guardan los elementos del DOM en la sección main, se reemplazaron variables comunes por un objeto y una función para simplificar el proceso de acceder al DOM y aumentar la legibilidad y mantenibilidad del código

--- src/public/scripts/main_script.js
+++ src/public/scripts/main_script.js
@@ -1,24 +1,36 @@
-const lButton = document.querySelector(".l-aside__button");
-const rButton = document.querySelector(".r-aside__button");
+let mainDOM = {};
+function createMainDOM() {
+  for (let i = 0; i < arguments.length; i++) {	
+    mainDOM[arguments[i]] = document.querySelector(arguments[i]);
+  }
+}
 const lAside = document.querySelector(".l-aside");
 const rAside = document.querySelector(".r-aside");
 const main = document.querySelector(".main");
+createMainDOM(
+	".aside__button",
+	".l-aside__button",
+	".r-aside__button",
+	".l-aside",
+	".r-aside",
+	".main"
+);
 var rButtonFlag = true;
 var lButtonFlag = true;
 function lButtonFunc() {
 	rButtonFlag = false;
-	let lAsideFlexVal = window.getComputedStyle(lAside).getPropertyValue('flex');
+	let lAsideFlexVal = window.getComputedStyle(mainDOM[".l-aside"]).getPropertyValue('flex');
 	if (lAsideFlexVal === "0 1 0%") {
-		main.style.flex = "4";	
-		lAside.style.flex = "1";
-		rAside.style.flex = "0";
+		mainDOM[".main"].style.flex = "4";	
+		mainDOM[".l-aside"].style.flex = "1";
+		mainDOM[".r-aside"].style.flex = "0";
 		document.querySelector(".l-aside__button img").style.transform = "scaleX(-1)";
 		document.querySelector(".r-aside__button img").style.transform = "scaleX(1)";
 	}
 	else {
-		lAside.style.flex = "0";
-		rAside.style.flex = "0";
-		main.style.flex = "1";
+		mainDOM[".l-aside"].style.flex = "0";
+		mainDOM[".r-aside"].style.flex = "0";
+		mainDOM[".main"].style.flex = "1";
 		document.querySelector(".l-aside__button img").style.transform = "scaleX(1)";
 	}
 	setTimeout(() => {
@@ -29,24 +41,24 @@ function rButtonFunc() {
 	lButtonFlag = false;
 	let rAsideFlexVal = window.getComputedStyle(rAside).getPropertyValue('flex');
 	if (rAsideFlexVal === "0 1 0%") {
-		main.style.flex = "8";	
-		rAside.style.flex = "1";
-		lAside.style.flex = "0";
+		mainDOM[".main"].style.flex = "8";	
+		mainDOM[".r-aside"].style.flex = "1";
+		mainDOM[".l-aside"].style.flex = "0";
 		document.querySelector(".r-aside__button img").style.transform = "scaleX(-1)";
 		document.querySelector(".l-aside__button img").style.transform = "scaleX(1)";
 	}
 	else {
-		rAside.style.flex = "0";
-		lAside.style.flex = "0";
-		main.style.flex = "1";
+		mainDOM[".r-aside"].style.flex = "0";
+		mainDOM[".l-aside"].style.flex = "0";
+		mainDOM[".main"].style.flex = "1";
 		document.querySelector(".r-aside__button img").style.transform = "scaleX(1)";
 	}
 	setTimeout(() => {
 		lButtonFlag = true;
 	}, 100);
 }
-lButton.addEventListener('click', lButtonFunc);
-rButton.addEventListener('click', rButtonFunc);
+mainDOM[".l-aside__button"].addEventListener('click', lButtonFunc);
+mainDOM[".r-aside__button"].addEventListener('click', rButtonFunc);
 document.addEventListener('keydown', (event) => {
 	if ((event.key === 'Q' || event.key === 'q') && lButtonFlag) {
 		lButtonFunc();
