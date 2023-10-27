e1326ecdc5e074254bfae5e31efc046f02e95ace
Nazareno Tognoli <nazarenotognoli@gmail.com>
2023-10-27 02:20:26 -0300

    Se añadió en la navegación derecha del main: Lógica del cambio de diseño activo e inactivo de los botones, lógica de como interactuan los botones y su contenido al desplegarse o contraerse la sección de navegación r-aside

--- src/public/scripts/main_script.js
+++ src/public/scripts/main_script.js
@@ -1,70 +1,107 @@
-let mainDOM = {};
-function createMainDOM() {
-  for (let i = 0; i < arguments.length; i++) {	
-    mainDOM[arguments[i]] = document.querySelector(arguments[i]);
+
+//Manipulción del DOM
+
+function createDOM(objeto, all, ...elementos) {
+  for (let i = 0; i < elementos.length; i++) {
+  	if (all) {
+  		let elementosEncontrados = document.querySelectorAll(elementos[i]);
+      	objeto[elementos[i]] = elementosEncontrados;
+  	}
+  	else {
+		objeto[elementos[i]] = document.querySelector(elementos[i]);
+  	}	
   }
 }
-const lAside = document.querySelector(".l-aside");
-const rAside = document.querySelector(".r-aside");
-const main = document.querySelector(".main");
-createMainDOM(
-	".aside__button",
-	".l-aside__button",
-	".r-aside__button",
-	".l-aside",
-	".r-aside",
-	".main"
+
+let mainDOM = {};
+
+//Elemento específico
+createDOM(
+	mainDOM,
+	false,
+  	".l-aside__button",
+  	".r-aside__button",
+  	".l-aside",
+  	".r-aside",
+  	".main",
+  	".l-aside__button img",
+	".r-aside__button img",
+  
 );
-var rButtonFlag = true;
-var lButtonFlag = true;
+//Todos los elementos
+createDOM(
+	mainDOM,
+	true,
+	".r-aside__section",
+	".r-aside__section span"
+);
+
+//Lógica de los botones laterales para abrir los Asides
 function lButtonFunc() {
-	rButtonFlag = false;
 	let lAsideFlexVal = window.getComputedStyle(mainDOM[".l-aside"]).getPropertyValue('flex');
 	if (lAsideFlexVal === "0 1 0%") {
 		mainDOM[".main"].style.flex = "4";	
 		mainDOM[".l-aside"].style.flex = "1";
 		mainDOM[".r-aside"].style.flex = "0";
-		document.querySelector(".l-aside__button img").style.transform = "scaleX(-1)";
-		document.querySelector(".r-aside__button img").style.transform = "scaleX(1)";
+		mainDOM[".l-aside__button img"].style.transform = "scaleX(-1)";
+		mainDOM[".r-aside__button img"].style.transform = "scaleX(1)";
+		for (let i = 0; i < mainDOM[".r-aside__section span"].length; i++) {
+			mainDOM[".r-aside__section"][i].style.opacity = "0";
+		}
 	}
 	else {
 		mainDOM[".l-aside"].style.flex = "0";
 		mainDOM[".r-aside"].style.flex = "0";
 		mainDOM[".main"].style.flex = "1";
-		document.querySelector(".l-aside__button img").style.transform = "scaleX(1)";
+		mainDOM[".l-aside__button img"].style.transform = "scaleX(1)";
+		for (let i = 0; i < mainDOM[".r-aside__section span"].length; i++) {
+			mainDOM[".r-aside__section"][i].style.opacity = "0";
+		}
 	}
-	setTimeout(() => {
-		rButtonFlag = true;
-	}, 100);
 }
 function rButtonFunc() {
-	lButtonFlag = false;
-	let rAsideFlexVal = window.getComputedStyle(rAside).getPropertyValue('flex');
+	let rAsideFlexVal = window.getComputedStyle(mainDOM[".r-aside"]).getPropertyValue('flex');
 	if (rAsideFlexVal === "0 1 0%") {
 		mainDOM[".main"].style.flex = "8";	
 		mainDOM[".r-aside"].style.flex = "1";
 		mainDOM[".l-aside"].style.flex = "0";
-		document.querySelector(".r-aside__button img").style.transform = "scaleX(-1)";
-		document.querySelector(".l-aside__button img").style.transform = "scaleX(1)";
+		mainDOM[".r-aside__button img"].style.transform = "scaleX(-1)";
+		mainDOM[".l-aside__button img"].style.transform = "scaleX(1)";
+		for (let i = 0; i < mainDOM[".r-aside__section span"].length; i++) {
+			mainDOM[".r-aside__section"][i].style.opacity = "1";
+		}
 	}
 	else {
 		mainDOM[".r-aside"].style.flex = "0";
 		mainDOM[".l-aside"].style.flex = "0";
 		mainDOM[".main"].style.flex = "1";
-		document.querySelector(".r-aside__button img").style.transform = "scaleX(1)";
+		mainDOM[".r-aside__button img"].style.transform = "scaleX(1)";
+		for (let i = 0; i < mainDOM[".r-aside__section span"].length; i++) {
+			mainDOM[".r-aside__section"][i].style.opacity = "0";
+		}
 	}
-	setTimeout(() => {
-		lButtonFlag = true;
-	}, 100);
 }
+//Clicks para laterales
 mainDOM[".l-aside__button"].addEventListener('click', lButtonFunc);
 mainDOM[".r-aside__button"].addEventListener('click', rButtonFunc);
+//Teclas para leterales
 document.addEventListener('keydown', (event) => {
-	if ((event.key === 'Q' || event.key === 'q') && lButtonFlag) {
+	if (event.key === 'Q' || event.key === 'q') {
 		lButtonFunc();
 	}
-	if ((event.key === 'E' || event.key === 'e') && rButtonFlag) {
+	if (event.key === 'E' || event.key === 'e') {
 		rButtonFunc();
 	}
 });
 
+//Lógica de los botones de r-aside
+
+function rAsideBTN () {
+  for (let i = 0; i < mainDOM[".r-aside__section"].length; i++) {
+  	mainDOM[".r-aside__section"][i].classList.remove("r-aside__section--active");
+  }
+  this.classList.add("r-aside__section--active");
+}
+for (let i = 0; i < mainDOM[".r-aside__section"].length; i++) {
+	mainDOM[".r-aside__section"][i].addEventListener('click', rAsideBTN);
+}
