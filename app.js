819fe2dd04c6b02cc8a5eaf4330ca53c013ae98c
Nazareno Tognoli <nazarenotognoli@gmail.com>
2023-11-14 01:51:30 -0300

    app.js modified for serving ISH.html view

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-let defaultView;
+let viewDefault;
+let viewISH;
 try {
-  defaultView = fs.readFileSync('./views/default.html', 'utf8');
-  console.log("File Succesfully Readed (defaultView)");
+  viewDefault = fs.readFileSync('./views/default.html', 'utf8');
+  console.log("File Succesfully Readed (viewDefault)");
+} catch (error) {
+  console.error('Error al leer el archivo:', error);
+}
+try {
+  viewISH = fs.readFileSync('./views/ISH.html', 'utf8');
+  console.log("File Succesfully Readed (viewISH)");
 } catch (error) {
   console.error('Error al leer el archivo:', error);
 }
 app.use(express.static('./public'));
 app.post('/default', (req, res) => {
-	res.json(defaultView);
+	res.json(viewDefault);
+});
+app.post('/ISH', (req, res) => {
+  res.json(viewISH);
 });
 
 app.listen(5000, () => {
