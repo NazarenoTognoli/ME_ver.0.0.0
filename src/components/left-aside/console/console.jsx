59f1f944a715246c1aee96d689cf3b83a8e344f3
Nazareno Tognoli <nazarenotognoli@gmail.com>
2024-03-26 19:02:52 -0300

    Added react icons and applied to console buttons

--- src/components/left-aside/console/console.jsx
+++ src/components/left-aside/console/console.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './console.css'
-function Console() {
+import { BsTerminal } from "react-icons/bs";
+import { VscAdd } from "react-icons/vsc";function Console() {
     return (
         <div className="console">
             <header className="console__header">
                 <div className="scrollbar-custom">
-                    <button id="console-button"></button>
-                    <button id="add-template-button"></button>
+                    <button id="console-button">
+                        <BsTerminal className="icon" />
+                    </button>
+                    <button id="add-template-button">
+                        <VscAdd className="icon" />
+                    </button>
                 </div>
             </header>
             <div className="console__body">
